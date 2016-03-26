8e3434a71105485c5611360572db989356860286
Vitek Pliska <vitek@creatiweb.cz>
2016-03-26 13:59:07 +0100

    proj init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # merk-api-py
 Merk API Python client
+
+## Installation
+
+    pip install merk-api
+
---
+++ setup.py
@@ -0,0 +1,50 @@
+from os.path import join, dirname
+from setuptools import setup
+
+import merkapi
+
+
+def parse_reqs(f='requirements.pip'):
+    ret = []
+    with open(join(dirname(__file__), f)) as fp:
+        for l in fp.readlines():
+            l = l.strip()
+            if l and not l.startswith('#'):
+                ret.append(l)
+    return ret
+
+setup_requires = ['setuptools']
+install_requires, tests_require = parse_reqs(), parse_reqs('requirements-test.pip')
+
+with open('README.md') as readmefile:
+    long_description = readmefile.read()
+
+setup(
+    name='merk-api',
+    version=merkapi.__versionstr__,
+    description='Merk API Python client',
+    long_description=long_description,
+    author='Vitek Pliska',
+    author_email='vitek@creatiweb.cz',
+    license='BSD',
+    url='https://api.merk.cz/docs/',
+
+    packages=('merkapi',),
+    include_package_data=True,
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Utilities",
+    ],
+
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+)
